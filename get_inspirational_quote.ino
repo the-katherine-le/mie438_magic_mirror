d94d6b282f896df0482567f0eafe74df4ea2ca2c
the-katherine-le <123709972+the-katherine-le@users.noreply.github.com>
2023-03-15 16:18:00 -0400

    Update get_inspirational_quote.ino

--- get_inspirational_quote.ino
+++ get_inspirational_quote.ino
@@ -1,73 +1,54 @@
-#include <WiFi.h>
 #include <HTTPClient.h>
-#include <Arduino_JSON.h>
+#include <ArduinoJson.h>   
 
-const char* ssid = "Katherine";
-const char* password = "password123";
-
-unsigned long lastTime = 0;
-unsigned long timerDelay = 10000;
-
-String jsonBuffer;
+#define WIFI_SSID "Katherine"
+#define WIFI_PASSWORD "password123"
+#define WIFI_TIMEOUT 30
 
 void setup() {
+  // put your setup code here, to run once:
+
   Serial.begin(115200);
+  //while(!Serial);
 
-  WiFi.begin(ssid, password);
-  Serial.println("Connecting");
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
+  Serial.print("Connecting to WiFi ");
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  int counter = 0;
+  while (WiFi.status() != WL_CONNECTED && counter < WIFI_TIMEOUT) {
+    delay(1000);
     Serial.print(".");
+    counter++;
   }
-  Serial.println("");
-  Serial.print("Connected to WiFi network with IP Address: ");
-  Serial.println(WiFi.localIP());
-
-  Serial.println("Timer set to 10 seconds (timerDelay variable), it will take 10 seconds before publishing the first reading.");
   
-  //to be called once and stored into memory
   String quote, author;
-  if (WiFi.status() == WL_CONNECTED)
+  if(WiFi.status() == WL_CONNECTED)
   {
     Serial.println("connected");
-    getQuote(quote, author);
+    getQuote(quote, author);  
   }
   else
   {
     quote = "WiFi connection timed out, try again later";
     Serial.println(quote);
   }
+  
+//  printQuote(String("\"") + quote + String("\""));
+//  printAuthor(author);
+//  printOther("adafruit.com/quotes");
+  
+//  epd.display();
+//  Serial.println("done, going to sleep...");
+//  // power down ePaper display
+//  epd.powerDown();
+//  // put microcontroller to sleep, wake up after specified time
+//  ESP.deepSleep(SLEEP * 1e6);
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if ((millis() - lastTime) > timerDelay) {
-    // Check WiFi connection status
-    if (WiFi.status() == WL_CONNECTED) {
-      String serverPath = "https://www.adafruit.com/api/quotes.php";
+  //Serial.println(quote);
 
-      //https://learn.adafruit.com/epaper-display-featherwing-quote-display/arduino-setup
-      //https://www.adafruit.com/api/quotes.php
-
-      jsonBuffer = httpGETRequest(serverPath.c_str());
-      Serial.println(jsonBuffer);
-      JSONVar myObject = JSON.parse(jsonBuffer);
-
-      // JSON.typeof(jsonVar) can be used to get the type of the var
-      if (JSON.typeof(myObject) == "undefined") {
-        Serial.println("Parsing input failed!");
-        return;
-      }
-      Serial.print("JSON object = ");
-      Serial.println(myObject);
-      Serial.print("Quote ");
-      Serial.println(myObject["quotes"]["quote"]);
-    }
-    else {
-      Serial.println("WiFi Disconnected");
-    }
-    lastTime = millis();
-  }
 }
 
 String getURLResponse(String url)
@@ -75,7 +56,7 @@ String getURLResponse(String url)
   HTTPClient http;
   String jsonstring = "";
   Serial.println("getting url: " + url);
-  if (http.begin(url))
+  if(http.begin(url))
   {
     Serial.print("[HTTP] GET...\n");
     // start connection and send HTTP header
@@ -88,11 +69,11 @@ String getURLResponse(String url)
 
       // file found at server
       if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) {
-        jsonstring = http.getString();
-        // use this string for testing very long quotes
-        //jsonstring = "[{\"text\":\"Don't worry about what anybody else is going to do… The best way to predict the future is to invent it. Really smart people with reasonable funding can do just about anything that doesn't violate too many of Newton's Laws!\",\"author\":\"Alan Kay\"}]";
-        Serial.println(jsonstring);
-      }
+      jsonstring = http.getString();
+      // use this string for testing very long quotes
+      //jsonstring = "[{\"text\":\"Don't worry about what anybody else is going to do… The best way to predict the future is to invent it. Really smart people with reasonable funding can do just about anything that doesn't violate too many of Newton's Laws!\",\"author\":\"Alan Kay\"}]";
+      Serial.println(jsonstring);
+    }
     } else {
       Serial.println("[HTTP] GET... failed, error: " + http.errorToString(httpCode));
     }
@@ -109,13 +90,13 @@ void getQuote(String &quote, String &author)
   StaticJsonDocument<1024> doc;
   String url = "https://www.adafruit.com/api/quotes.php";
   String jsonquote = getURLResponse(url);
-  if (jsonquote.length() > 0)
+  if(jsonquote.length() > 0)
   {
     // remove start and end brackets, jsonBuffer is confused by them
-    jsonquote = jsonquote.substring(1, jsonquote.length() - 1);
+    jsonquote = jsonquote.substring(1,jsonquote.length()-1);
     Serial.println("using: " + jsonquote);
     DeserializationError error = deserializeJson(doc, jsonquote);
-    if (error)
+    if (error) 
     {
       Serial.println("json parseObject() failed");
       Serial.println("bad json: " + jsonquote);
@@ -127,37 +108,13 @@ void getQuote(String &quote, String &author)
       String tauthor = doc["author"];
       quote = tquote;
       author = tauthor;
+
+      Serial.println(quote);
+      Serial.println(author);
     }
   }
   else
   {
     quote = "Error retrieving URL";
   }
-}
-
-String httpGETRequest(const char* serverName) {
-  WiFiClient client;
-  HTTPClient http;
-
-  // Your Domain name with URL path or IP address with path
-  http.begin(client, serverName);
-
-  // Send HTTP POST request
-  int httpResponseCode = http.GET();
-
-  String payload = "{}";
-
-  if (httpResponseCode > 0) {
-    Serial.print("HTTP Response code: ");
-    Serial.println(httpResponseCode);
-    payload = http.getString();
-  }
-  else {
-    Serial.print("Error code: ");
-    Serial.println(httpResponseCode);
-  }
-  // Free resources
-  http.end();
-
-  return payload;
 }
