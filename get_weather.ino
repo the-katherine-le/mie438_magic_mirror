8d4cfc0d38b93be8ebc959cdddb94616932f8b54
the-katherine-le <123709972+the-katherine-le@users.noreply.github.com>
2023-03-27 10:05:50 -0400

    Update get_weather.ino

--- get_weather.ino
+++ get_weather.ino
@@ -1,6 +1,6 @@
 #include <WiFi.h>
 #include <HTTPClient.h>
-#include <Arduino_JSON.h>
+#include <ArduinoJson.h> 
 #include <TFT_eSPI.h>
 #include <SPI.h>
 TFT_eSPI tft = TFT_eSPI();
@@ -26,6 +26,9 @@ unsigned long lastTime = 0;
 unsigned long timerDelay = 10000;
 bool initWeatherQuery = false;
 String temp, pressure, humidity, wind, description;
+//float temp, wind;
+//int humidity, pressure;
+//String description;
 
 void setup() {
   Serial.begin(115200);
@@ -42,26 +45,26 @@ void setup() {
   Serial.println("Timer set to 10 seconds (timerDelay variable), it will take 10 seconds before publishing the first reading.");
 
   //Initialize touch screen
-//  tft.begin();
-//  tft.setRotation(1); //Horizontal
-//  tft.fillScreen(BLACK);
+  //  tft.begin();
+  //  tft.setRotation(1); //Horizontal
+  //  tft.fillScreen(BLACK);
 }
 
 void loop() {
   if ((millis() - lastTime) > timerDelay) {
     if (WiFi.status() == WL_CONNECTED) {
       Serial.println("Getting weather data...");
-      getWeather(temp, pressure, humidity, wind, description); 
+      getWeather(temp, pressure, humidity, wind, description);
       Serial.print("Temp: ");
-      Serial.println(temp);
+      Serial.println(String(temp));
       Serial.print("Pressure: ");
-      Serial.println(pressure);
+      Serial.println(String(pressure));
       Serial.print("Humidity: ");
-      Serial.println(humidity);
+      Serial.println(String(humidity));
       Serial.println("Wind Speed: ");
-      Serial.print(wind);
+      Serial.print(String(wind));
       Serial.print("Description");
-      Serial.println(description); 
+      Serial.println(description);
     }
     else {
       Serial.println("Wifi disconnect...");
@@ -109,19 +112,27 @@ String getURLResponse(String url)
 
 void getWeather(String &temp, String &pressure, String &humidity, String &wind, String &description)
 {
-
-  DynamicJsonDocument(1024) doc;
+  // The filter: it contains "true" for each value we want to keep
+  StaticJsonDocument<500> filter;
+  filter["list"][0]["main"]["temp"] = true;
+  filter["list"][0]["main"]["humidity"] = true;
+  filter["list"][0]["main"]["pressure"] = true;
+  filter["list"][0]["wind"]["speed"] = true;
+  filter["list"][0]["weather"][0]["description"] = true;
+  
+  StaticJsonDocument<1024> doc;
   String url = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "," + countryCode + "&APPID=" + openWeatherMapApiKey + "&units=metric";
   String jsonWeather = getURLResponse(url);
-  
+
   if (jsonWeather.length() > 0)
   {
     // remove start and end brackets, jsonBuffer is confused by them
-    jsonWeather = jsonWeather.substring(1, jsonWeather.length() - 1);
+    //jsonWeather = jsonWeather.substring(1, jsonWeather.length() - 1);
 
     Serial.println("using: " + jsonWeather);
-
-    DeserializationError error = deserializeJson(doc, jsonWeather);
+    
+    DeserializationError error = deserializeJson(doc, jsonWeather,DeserializationOption::Filter(filter));
+    serializeJsonPretty(doc, Serial); //output results
 
     if (error)
     {
@@ -131,17 +142,25 @@ void getWeather(String &temp, String &pressure, String &humidity, String &wind, 
     }
     else
     {
-      float t_temp = (float)(doc["main"]["temp"]);        // get temperature
-      int   t_humidity = doc["main"]["humidity"];                  // get humidity
-      float t_pressure = (float)(doc["main"]["pressure"]) / 1000;  // get pressure
-      float t_wind_speed = doc["wind"]["speed"];                   // get wind speed
+//      float t_temp = (float)(doc["main"]["temp"]);        // get temperature
+//      int   t_humidity = doc["main"]["humidity"];                  // get humidity
+//      float t_pressure = (float)(doc["main"]["pressure"]) / 1000;  // get pressure
+//      float t_wind_speed = doc["wind"]["speed"];                   // get wind speed
+//      String t_description = doc["weather"][0]["description"];
+      
+      //ISSUE: THIS IS STILL OUTPUTTING AS '0' IN THE SERIAL MONITOR
+
+      String t_temp = doc["main"]["temp"];        // get temperature
+      String t_humidity = doc["main"]["humidity"];                  // get humidity
+      String t_pressure = doc["main"]["pressure"];  // get pressure
+      String t_wind_speed = doc["wind"]["speed"];                   // get wind speed
       String t_description = doc["weather"][0]["description"];
 
-      temp = t_temp.as<String>();
-      humidity = t_humidity.as<String>();
-      pressure = t_pressure.as<String>();
-      wind = t_wind.as<String>();
-      description = t_description.as<String>();
+      temp = t_temp;
+      humidity = t_humidity;
+      pressure = t_pressure;
+      wind = t_wind_speed;
+      description = t_description;
     }
   }
   else
